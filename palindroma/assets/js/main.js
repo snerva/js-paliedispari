0383f8243c3aa9623ce2783710d77ef7aecaa422
snerva <selenenerva@live.it>
2022-10-14 13:27:35 +0200

    palindrome function

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,5 +1,13 @@
 //Chiedere all’utente di inserire una parola con variabile e prompt
+//rendere la parola leggibile da entrambe i versi
 //Creare una funzione per capire se la parola inserita è palindroma 
+
+/*
+split
+reverse
+join
+if
+*/ 
 
 const userWord = prompt('Inserisci una parola:');
 console.log(userWord);
@@ -12,3 +20,15 @@ console.log(wordArReverse);
 
 const wordReverse = wordArReverse.join('');
 console.log(wordReverse);
+
+function palindrome(userWord){
+    if (userWord === wordReverse){
+       return true;
+    } 
+}
+
+if (palindrome(userWord)){
+    console.log('Hai inserito una parola palindroma');
+} else {
+    console.log('Hai inserito una parola comune');
+}
