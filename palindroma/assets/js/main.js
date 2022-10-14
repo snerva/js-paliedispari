67a5bd011617d5232f7f8033e0692a3495858d4b
snerva <selenenerva@live.it>
2022-10-14 12:01:01 +0200

    comments

---
+++ palindroma/assets/js/main.js
@@ -0,0 +1,8 @@
+//Chiedere all’utente di inserire una parola con variabile e prompt
+//dividere la lunghezza della parola per due per vedere se la prima parte e la seconda sono uguali con lenght e for 
+// se sono uguali é true se no é false 
+//Creare una funzione per capire se la parola inserita è palindroma 
+
+const userWord = prompt('Inserisci una parola:');
+
+
