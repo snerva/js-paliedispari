e18e4eb4d865e32f017fd063fb1ee0b42a9f60dd
snerva <selenenerva@live.it>
2022-10-14 15:11:09 +0200

    fuction pcnumber and winner

--- paliedispari/assets/js/main.js
+++ paliedispari/assets/js/main.js
@@ -8,3 +8,34 @@ prompt
 function math.random
 function sum if
 */
+
+const userPariDispari = prompt('Scegli pari o dispari:');
+const userNumber =  prompt('Scegli un numero da 1 a 5:');
+
+function randomNumber(min,max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+const pcNumber = randomNumber(0,6);
+console.log(pcNumber);
+
+function sumNumbers(userNumber, pcNumber){
+    const sum = userNumber + pcNumber;
+    if (sum / 2 == 0){
+      return true;
+    } 
+}
+
+
+let vincitore;
+if (sumNumbers(userNumber, pcNumber)){
+    vincitore = 'pari';
+} else {
+    vincitore = 'dispari';
+}
+
+if (vincitore == userPariDispari){
+    document.querySelector('h1').innerHTML = 'Hai vinto!';
+} else {
+    document.querySelector('h1').innerHTML = 'Ha vinto il computer!';
+}
+
